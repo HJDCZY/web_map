36820cd9dca1703bad6ee59e371c07001855d8e1
zhi-feng2008 <z1258909489@outlook.com>
2024-01-30 00:09:30 +0800

    添加解析输出
    添加解析输出

--- client/script.js
+++ client/script.js
@@ -11,10 +11,21 @@ websocket.onerror = function (evt) {
 let playerInfo = document.createElement('div');
 var count  = 0;
 websocket.onmessage = function (evt) {
-    console.log('Retrieved data from server: ' + evt.data);
-    let players = JSON.parse(evt.data);
 
-    // 在这里处理数据...
+    let players = JSON.parse(evt.data);
+    
+    for (let i = 0; i < playerData.length; i++) {
+        let player = playerData[i];
+
+        console.log("Player Number: " + i);
+        console.log("Server ID: " + player.serverid);
+        console.log("Player Name: " + player.playername);
+        console.log("Coordinates (X, Y, Z): " + player.croodx + ", " + player.croody + ", " + player.croodz);
+        console.log("In Plane: " + (player.inplane === 1 ? "Yes" : "No"));
+        console.log("Speed: " + player.speed);
+
+        // 在这里处理每个玩家的数据...
+    }
     
     
      count +=1;
