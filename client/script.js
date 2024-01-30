db3c17b87e9e9a787d923b90c374549ccc5261c0
zhi-feng2008 <z1258909489@outlook.com>
2024-01-30 18:28:21 +0800

    完成用户位置显示
    完成用户位置显示

--- client/script.js
+++ client/script.js
@@ -6,17 +6,18 @@ websocket.onclose = function (evt) {
     console.log("Disconnected");
 };
 websocket.onerror = function (evt) {
-    console.log("Error to connect to websocket server");
-};  
-let playerInfo = document.createElement('div');
-var count  = 0;
+    console.log("Error to connect to WebSocket server");
+};
+
+var mapContainer = document.getElementById('mapContainer'); // 获取地图容器
+
 websocket.onmessage = function (evt) {
+    var playersData = JSON.parse(evt.data);
 
-    let players = JSON.parse(evt.data);
-    
-    for (let i = 0; i < playerData.length; i++) {
-        let player = playerData[i];
+    // 清空地图容器，以便重新添加玩家位置
+    mapContainer.innerHTML = '';
 
+    playersData.forEach(function(player, i) {
         console.log("Player Number: " + i);
         console.log("Server ID: " + player.serverid);
         console.log("Player Name: " + player.playername);
@@ -24,9 +25,25 @@ websocket.onmessage = function (evt) {
         console.log("In Plane: " + (player.inplane === 1 ? "Yes" : "No"));
         console.log("Speed: " + player.speed);
 
-        // 在这里处理每个玩家的数据...
-    }
-    
-    
-     count +=1;
+        // 创建玩家名称标签
+        var playerLabel = document.createElement('div');
+        playerLabel.className = 'player-label';
+        playerLabel.innerHTML = player.playername;
+
+        // 将玩家坐标映射到百分比，适应地图容器的大小
+        var leftPercentage = mapCoordinateToPercentage(player.croodx, -10000, 10000);
+        var topPercentage = mapCoordinateToPercentage(player.croody, -10000, 10000);
+
+        // 设置玩家名称标签的位置
+        playerLabel.style.left = leftPercentage + '%';
+        playerLabel.style.top = topPercentage + '%';
+
+        // 将玩家名称标签添加到地图容器中
+        mapContainer.appendChild(playerLabel);
+    });
 };
+
+// 将地图坐标映射到百分比
+function mapCoordinateToPercentage(coordinate, min, max) {
+    return ((coordinate - min) / (max - min)) * 100;
+}
